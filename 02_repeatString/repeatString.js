9c57ef838a25a74ab597f4d4288a47654f97ea54
sykes <kanekikenrus@mail.ru>
2024-11-04 20:02:10 +0500

    Got this shit sorted out

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR'
+    }
+    finalString = ''
+    for (let i = 0; i < num; i++) {
+        finalString += string
+    }
+    return finalString
 };
+console.log(repeatString('asd', 2))
 
 // Do not edit below this line
 module.exports = repeatString;
